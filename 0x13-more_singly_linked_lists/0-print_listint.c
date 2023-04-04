1b2e4adcaa15fae2370c547109700e45358a04be
emlincs <emlincs09@gmail.com>
2023-04-04 02:17:05 -0700

    More singly linked lists

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,14 +8,14 @@
  */
 size_t print_listint(const listint_t *h)
 {
-size_t nodes = 0;
+size_t points = 0;
 
 while (h)
 {
 	printf("%d\n", h->n);
 	h = h->next;
-	nodes++;
+	++points;
 }
-return (nodes);
+return (points);
 }
 
